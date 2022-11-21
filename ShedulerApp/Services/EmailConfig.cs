a8e24acc74a502c0e217db78ba12bdd71e789f76
NKazus <114696053+NKazus@users.noreply.github.com>
2022-11-21 19:13:46 +0300

    Update EmailConfig.cs

--- ShedulerApp/Services/EmailConfig.cs
+++ ShedulerApp/Services/EmailConfig.cs
@@ -2,10 +2,10 @@
 {
     public static class EmailConfig
     {
-        public static string From { get; } = "isamaneeva@gmail.com";
+        public static string From { get; } = "temp@gmail.com";
         public static string SmtpServer { get; } = "smtp.gmail.com";
         public static int Port { get; } = 465;
-        public static string Username { get; } = "isamaneeva@gmail.com";
-        public static string Password { get; } = "grtnkrkgivyghgfq";
+        public static string Username { get; } = "temp@gmail.com";
+        public static string Password { get; } = "*********";
     }
 }
